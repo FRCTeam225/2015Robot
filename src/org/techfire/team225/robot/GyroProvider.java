b10005ec1b59accb838dea6961c44a6b1f33ae28
225 Programmer <awolnikowski@gmail.com>
2015-02-08 15:20:11 -0500

    Minor gyro code

--- src/org/techfire/team225/robot/GyroProvider.java
+++ src/org/techfire/team225/robot/GyroProvider.java
@@ -26,6 +26,7 @@ public class GyroProvider extends Thread {
 		try {
 			int port = 1337;
 			
+			@SuppressWarnings("resource")
 			DatagramSocket dsocket = new DatagramSocket(port);
 			
 			while ( true )
