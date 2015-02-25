6dca3fc915b1cb4fac2f366f34cc9570e80acc8e
225 Programmer <awolnikowski@gmail.com>
2015-02-24 19:29:50 -0500

    Small Constants change, needs tuning

--- src/org/techfire/team225/robot/CompetitionRobotConstants.java
+++ src/org/techfire/team225/robot/CompetitionRobotConstants.java
@@ -20,9 +20,9 @@ public class CompetitionRobotConstants {
     public static int ARM_POT = 1; // Analog
     public static int WINGS_SOLENOID = 2; // PCM
     public static int FLOOR_POSITION = 2260; //1950;
-    public static int FIRST_POSITION = 2070-355;
-    public static int POST_CONTAINER_POSITION = 2320;
+    public static int FIRST_POSITION = FLOOR_POSITION + 300;
     public static int TOP_POSITION = 3364; //3000;
+    public static int POST_CONTAINER_POSITION = TOP_POSITION - 300;
     
     // gripper
     public static int GRIPPER_SOLENOID_LEFT = 1; // PCM  
